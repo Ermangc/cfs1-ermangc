556eff7b9f7d03d2f89c01a055ee3c69f7680af3
erman garcia cano <ermangarciacano@Ermans-MacBook-Pro.local>
2024-02-29 21:25:53 -0300

    ejercicio clase 1

---
+++ clase 1/ejercicio1.ts
@@ -0,0 +1,15 @@
+//pasos para cocinar una torta
+
+console.log("Pasos para cocinar una torta:");
+console.log("1- Buscar ingredientes necesarios (huevos,azucar, aceite, harina, leche y escencia de vainilla)");
+console.log("2- Obtenerr un bowl y batidor");
+console.log("3- Romper 3 huevos, agregar una taza de azucar y escencia de vainilla, a gusto");
+console.log("4- Batir aproximadamente 1 minuto");
+console.log("5- Anadir aceite y mezclar");
+console.log("6- Agregar una taza de harina de a poco mientras bate, preferible harina leudante. ");
+console.log("7- Agregar leche y mezclar todo");
+console.log("8- Agregar una nueva taza de harina y batir por 1 minuto aproximadamente");
+console.log("9- Vertir el contenido en un molde previamente enharinado");
+console.log("10- Meter el molde en el horno, a fuego lento, por 50 minutos aproximadamente");
+console.log("11- Antes de retirar del horno, pinchar la torta con un cuchillo y si este sale limpio esta cocinada");
+console.log("12- Dejar enfriar 40 minutos antes de cortar");
