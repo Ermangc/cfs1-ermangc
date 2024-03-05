9bd809803d503dc56d95b05ac96efd197fcd1594
ermangarciacano@gmail.com <ermangarciacano@Ermans-MacBook-Pro.local>
2024-03-05 13:35:22 -0300

    se acomodo ejercicio2

--- clase2/ejercicio2.js
+++ clase2/ejercicio2.js
@@ -1,7 +1,12 @@
 //ejercicio: Calculo de Descuento.
 // codigo para obtener los datos que ingresa el usuario
 var realineSync = require("readline-sync");
-var precioProducto = realineSync.question("ingrese precio: ");
+var precioProducto = realineSync.questionInt("ingrese precio: ");
 // calculo del descuento
-var precioFinal = precioProducto - (precioProducto) * 0.1;
-console.log("Su precio es de $", precioFinal);
+if (precioProducto > 1000) {
+    var precioFinal = precioProducto - (precioProducto * 0.1);
+    console.log("Su precio es de $", precioFinal);
+}
+else {
+    console.log("Su precio es de $", precioProducto);
+}
--- clase2/ejercicio2.ts
+++ clase2/ejercicio2.ts
@@ -3,8 +3,15 @@
 // codigo para obtener los datos que ingresa el usuario
 const realineSync = require("readline-sync");
 
-let precioProducto : number = realineSync.question("ingrese precio: ");
+let precioProducto : number = realineSync.questionInt("ingrese precio: ");
 // calculo del descuento
-let precioFinal : number = precioProducto - (precioProducto) * 0.1;
 
-console.log("Su precio es de $",precioFinal);
+if(precioProducto > 1000) {
+    let precioFinal : number = precioProducto - (precioProducto * 0.1);
+    console.log("Su precio es de $",precioFinal);
+} else {
+    console.log("Su precio es de $",precioProducto);
+}
+
+
+
