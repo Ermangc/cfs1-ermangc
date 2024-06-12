f4d74a835d643f535c604e6ab23e39ac4c26ddb8
erman <ermangarciacano@gmail.com>
2024-06-12 14:32:45 -0300

    se termino ejercicio 5

--- frontend/clase5/js_ejercicio5.js
+++ frontend/clase5/js_ejercicio5.js
@@ -3,24 +3,32 @@ let bajaStock = document.getElementById("baja_stock");
 let altaStock = document.getElementById("alta_stock");
 let btnConfirmar = document.getElementById("btn-confirmar");
 let divLista = document.getElementById("div_lista");
+let itemsLista = document.getElementById("items");
 
 function onFocus(x) {
-  x.style.border = "none";
-  x.style.borderTop = "solid #999";
-  x.style.outline = "none";
-  x.style.textDecoration = "none";
-  x.style.color = "inherit";
+    x.style.border = "none";
+    x.style.borderTop = "solid #999";
+    x.style.outline = "none";
+    x.style.textDecoration = "none";
+    x.style.color = "inherit";
 }
+
+
 
 function onClick(){
     let nombre = nombreMedicamento.value;
     let bajaStocks = bajaStock.value;
     let altaStocks= altaStock.value;
+    console.log(`${nombre} dar de baja ${bajaStocks} unidades y de alta ${altaStocks} unidades.`);
+}
+
+function copiar(){
+    document.getElementById("copiar").innerHTML = "Copiaste la lista"
 }
 
 btnConfirmar.addEventListener("click", ()=>{
     onClick();
-    divLista = document.createElement("ul");
-    divLista.appendChild("ul") = document.createElement("li");
-    li.innerHTML = "`${nombre} + ${bajaStocks} + ${altaStocks}`";
-})
+    var li = document.createElement("li")
+    itemsLista.appendChild(li);
+    li.textContent = `${nombreMedicamento.value} dar de baja ${bajaStock.value} unidades y de alta ${altaStock.value} unidades.`;
+});
